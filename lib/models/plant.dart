f6ec3647b932497b9041a24235be5c7b7c83f92e
JULID-AB\julid_accelbyte <julid@accelbyte.net>
2022-06-26 20:52:55 +0700

    display plant data from json into a simple list

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
 class Plant {
   late String name;
   late String latinName;
@@ -18,4 +23,23 @@ class Plant {
     required this.minTemperature,
     required this.maxTemperature,
   });
+
+  static List<Plant> _cache = List.empty();
+
+  static Future<List<Plant>> fromFile() async {
+    if (_cache.isNotEmpty) {
+      return _cache;
+    }
+
+    var json = await rootBundle.loadString("data/plant.json");
+    List decodedJson = jsonDecode(json);
+    _cache = decodedJson.map((e) => Plant.fromJson(e)).toList();
+    // await Future.delayed(const Duration(seconds: 3));
+    return _cache;
+  }
+
+  Plant.fromJson(Map<String, dynamic> plant) {
+    name = plant["name"];
+    latinName = plant["latinName"];
+  }
 }
--- lib/pages/plantrecomendationpage.dart
+++ lib/pages/plantrecomendationpage.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class PlantRecomendationPage extends StatelessWidget {
+import '../models/plant.dart';
+
+class PlantRecomendationPage extends StatefulWidget {
+  const PlantRecomendationPage({Key? key}) : super(key: key);
+
+  @override
+  State<PlantRecomendationPage> createState() =>
+      _PlantRecommendationPageState();
+}
+
+class _PlantRecommendationPageState extends State<PlantRecomendationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Plant Recommendation Page"),
+    return FutureBuilder<List<Plant>>(
+      future: Plant.fromFile(),
+      builder: (context, snapshot) {
+        if (!isDone(snapshot.connectionState)) {
+          return const CircularProgressIndicator();
+        }
+
+        if (snapshot.hasError) {
+          // try to display error message
+        }
+
+        return ListView.builder(
+            itemCount: snapshot.data?.length ?? 0,
+            itemBuilder: (context, index) {
+              var plant = snapshot.data![index];
+
+              return ListTile(
+                title: Text(plant.name),
+                subtitle: Text(plant.latinName),
+              );
+            });
+      },
     );
   }
+
+  bool isDone(ConnectionState state) => state == ConnectionState.done;
 }
