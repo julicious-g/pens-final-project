39bb34325f012d2a3eb46f72704cfe1a952e2ba9
JULID-AB\julid_accelbyte <julid@accelbyte.net>
2022-10-20 22:07:26 +0700

    sensor_service : set sensor values as queue

--- lib/services/sensor/sensor_service.dart
+++ lib/services/sensor/sensor_service.dart
@@ -1,35 +1,51 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 abstract class SensorService extends ChangeNotifier {
-  late double _ph = 0;
-  late double _salinity = 0;
-  late int _moisture = 0;
-  late double _temperature = 0;
+  final Queue<double> _phs = Queue<double>();
+  final Queue<double> _salinities = Queue<double>();
+  final Queue<int> _moistures = Queue<int>();
+  final Queue<double> _temperatures = Queue<double>();
 
   @protected
   void setPh(double ph) {
-    _ph = ph;
+    _addToQueue(_phs, ph);
   }
 
   @protected
   void setSalinity(double salinity) {
-    _salinity = salinity;
+    _addToQueue(_salinities, salinity);
   }
 
   @protected
   void setMoisture(int moisture) {
-    _moisture = moisture;
+    _addToQueue(_moistures, moisture);
   }
 
   @protected
   void setTemperature(double temperature) {
-    _temperature = temperature;
+    _addToQueue(_temperatures, temperature);
   }
 
-  double getPh() => _ph;
-  double getSalinity() => _salinity;
-  double getTemperature() => _temperature;
-  int getMoisture() => _moisture;
+  double getPh() =>
+      _phs.reduce((value, element) => value + element) / _phs.length;
+  double getSalinity() =>
+      _salinities.reduce((value, element) => value + element) /
+      _salinities.length;
+  double getTemperature() =>
+      _temperatures.reduce((value, element) => value + element) /
+      _temperatures.length;
+  double getMoisture() =>
+      _moistures.reduce((value, element) => value + element) /
+      _salinities.length;
 
   Future<SensorService> init();
+
+  void _addToQueue<T>(Queue<T> list, T value) {
+    list.add(value);
+    if (list.length > 100) {
+      list.removeFirst();
+    }
+  }
 }
